a56b7a62990107d7f5f30e2e1fd1677224cd2bb5
Yoshinori Kanno <yoshinori.kanno@me.com>
2025-03-16 22:11:50 +0900

    Squareコンポーネントを追加し、ボードに配置してクリック時に状態を更新する機能を実装

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-export default function Square() {
+import { useState } from 'react';
+
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  const handleClick = () => {
+    setValue("X");
+    console.log(value);
+  }
+  return <button className="square" onClick={handleClick}>{value}</button>
+}
+
+
+export default function Board() {
   return (
     <>
-     <div className="board-row">
-     <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
 
-     </div>
-     <div className="board-row">
+      </div>
+      <div className="board-row">
 
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-     </div>
-     <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
 
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-     </div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
 
     </>
   );
